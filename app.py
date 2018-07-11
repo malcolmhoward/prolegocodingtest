257ba18cb82fa938136bc3fa017ba041e051cd55
m <malcolm.g.howard@gmail.com>
2018-07-11 07:42:41 -0400

    Added main Flask app file.

---
+++ app.py
@@ -0,0 +1,27 @@
+import os
+from flask import Flask
+
+app = Flask(__name__)
+
+POSTGRES = {
+    'user': os.getenv('DB_USERNAME'),
+    'pw': os.getenv('DB_PASSWORD'),
+    'db': os.getenv('DB_NAME'),
+    # For security reasons, secret/sensitive information should not be committed to a repository in a plain text file.
+    # You should configure these details using environment variables, and then import them like the example above.
+    # Environment variables can be created in the terminal as follows: $ export DB_USERNAME="database_username"
+    'host': 'localhost',
+    'port': '5432',
+}
+
+app.config['DEBUG'] = True
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://%(user)s:\
+%(pw)s@%(host)s:%(port)s/%(db)s' % POSTGRES
+
+
+@app.route("/")
+def main():
+    return 'Hello World !'
+
+if __name__ == '__main__':
+    app.run()
